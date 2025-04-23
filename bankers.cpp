b074564e1f3862eb21f0ad29d3cc7dd7549ba0bf
kwhitmoyer <katewhitmoyer@gmail.com>
2025-04-23 14:40:11 -0400

    Added one checking the safety of each process.

--- bankers.cpp
+++ bankers.cpp
@@ -8,6 +8,6 @@ int main(){
     calculateNeededResources(neededResources, maxResources, allocatedResources);
     printNeededResources(neededResources); 
 
-    
+    safetyAlgorithm(resourcesAvailable, neededResources); 
 
 }
--- helper.hpp
+++ helper.hpp
@@ -1,6 +1,38 @@
 #include "header.hpp"
 
 
+bool safetyAlgorithm(int resourcesAvailable[NUM_OF_RESOURCES], int resourceNeeded[NUM_OF_PROCESSES][NUM_OF_RESOURCES]){
+    int work[NUM_OF_RESOURCES] = {0, 0, 0};
+    for(int resourceNumber = 0; resourceNumber < NUM_OF_RESOURCES; ++resourceNumber){
+        work[resourceNumber] = resourcesAvailable[resourceNumber];
+    }
+
+    bool finish[NUM_OF_PROCESSES] = {false, false, false, false, false}; 
+
+    int processesFinished = 0;
+
+    while(processesFinished < NUM_OF_PROCESSES){
+        bool foundProcess = false; 
+        for(int processNumber = 0; processNumber < NUM_OF_PROCESSES; ++processNumber){
+            if (!finish[processNumber]){
+                bool allResourcesAvailable = false; 
+                for(int resourceType = 0; resourceType < NUM_OF_RESOURCES; ++resourceType){
+                    if(resourceNeeded[processNumber][resourceType] > work[resourceType]){
+                        allResourcesAvailable = false; 
+                        std::cout << "Unsafe sequence found for process: " << processNumber << std::endl;  
+                        break; 
+                    }
+                }
+                allResourcesAvailable = true; 
+                std::cout << "A safe process has been found for process: " << processNumber << std::endl;
+            }
+            ++processesFinished; //temp 
+        }
+    }
+    return true; 
+}
+
+
 void initalizeNeed(int neededResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES]){
     for(int processNumber = 0; processNumber < NUM_OF_PROCESSES; ++processNumber){
         for(int resourceNumber = 0; resourceNumber < NUM_OF_RESOURCES; ++resourceNumber){
