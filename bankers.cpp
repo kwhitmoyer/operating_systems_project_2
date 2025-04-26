96a487372f122153ae2c54aaff82658585a17d5e
kwhitmoyer <katewhitmoyer@gmail.com>
2025-04-26 14:24:08 -0400

    Added better log outputs

--- bankers.cpp
+++ bankers.cpp
@@ -5,9 +5,17 @@
 int main(){
     std::cout << "Begin banker's algorithm safety check." << std::endl; 
 
+    std::cout << "----------------------- [LOG]: Calculating Resources -----------------------" << std::endl << std::endl; 
     calculateNeededResources(neededResources, maxResources, allocatedResources);
     printNeededResources(neededResources); 
 
-    safetyAlgorithm(resourcesAvailable, neededResources); 
+    std::cout << "----------------------- [LOG]: Beginning Safety Check ----------------------" << std::endl << std::endl; 
+    bool safetyCheckResult = safetyAlgorithm(resourcesAvailable, neededResources); 
+    std::cout << "----------------------- [LOG]: Finished Safety Check ----------------------" << std::endl << std::endl; 
 
+    if (safetyCheckResult){
+        std::cout << "----------------------- [DECISION]: System is in a safe state! ----------------------" << std::endl << std::endl; 
+    } else {
+        std::cout << "----------------------- [DECISION]: System is in unsafe state!  ----------------------" << std::endl << std::endl; 
+    }
 }
