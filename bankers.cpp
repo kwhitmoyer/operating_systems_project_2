518a59324bd612e9c4f7ecfd9639b5672d2613d2
kwhitmoyer <katewhitmoyer@gmail.com>
2025-04-27 22:02:35 -0400

    Added text input

--- bankers.cpp
+++ bankers.cpp
@@ -5,6 +5,31 @@
 int main(){
     std::cout << "Begin banker's algorithm safety check." << std::endl; 
 
+    std::ifstream initialSystemState("inputdata.txt");
+    if(!initialSystemState) {
+        std::cout << "Can't open file" << std::endl; 
+    }
+
+    //Read in available resources
+    for(int resourceType = 0; resourceType < NUM_OF_RESOURCES; ++resourceType){
+        initialSystemState >> resourcesAvailable[resourceType];
+    }
+
+    //read in max resources per process 
+    for(int processNumber = 0; processNumber < NUM_OF_PROCESSES; ++processNumber){
+        for(int resourceType = 0; resourceType < NUM_OF_RESOURCES; ++resourceType){
+            initialSystemState >> maxResources[processNumber][resourceType];
+        }
+    }
+
+    //read in allocated resources per process 
+    for(int processNumber = 0; processNumber < NUM_OF_PROCESSES; ++processNumber){
+        for(int resourceType = 0; resourceType < NUM_OF_RESOURCES; ++resourceType){
+            initialSystemState >> allocatedResources[processNumber][resourceType];
+        }
+    }
+
+
     std::cout << "----------------------- [LOG]: Calculating Resources -----------------------" << std::endl << std::endl; 
     calculateNeededResources(neededResources, maxResources, allocatedResources);
     printNeededResources(neededResources); 
--- header.hpp
+++ header.hpp
@@ -2,6 +2,7 @@
 #define HEADER_CPP
 
 #include <iostream>
+#include <fstream>
 
 //Data Structures for the Banker's Algorithm
 const int NUM_OF_PROCESSES = 5;
@@ -10,7 +11,13 @@ const int NUM_OF_RESOURCES = 3;
 // P0, P1, P2, P3, P4, P
 int processes[] = {0, 1, 2, 3, 4};
 
-// A = 3, B = 3, C = 2
+int resourcesAvailable[NUM_OF_RESOURCES]; 
+int allocatedResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES];
+int maxResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES];
+
+int neededResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES]; 
+
+/* Resoure matrix should read from input file as follows: 
 int resourcesAvailable[NUM_OF_RESOURCES] = {3, 3, 2};
 
 int allocatedResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES] = {{0, 1, 0},
@@ -24,8 +31,7 @@ int maxResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES] = {{7, 5, 3},
                                                         {9, 0, 2},
                                                         {2, 2, 2},
                                                         {4, 3, 3}};
-
-int neededResources[NUM_OF_PROCESSES][NUM_OF_RESOURCES]; 
+*/
 
 
 #endif 
